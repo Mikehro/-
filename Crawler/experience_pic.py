b83f7242b5204c4fbf5dc490b0da26d4ae299e75
Sogger <56769300+SoggerWu@users.noreply.github.com>
2021-11-13 20:50:36 +0800

    Update experience_pic.py

--- Crawler/experience_pic.py
+++ Crawler/experience_pic.py
@@ -2,26 +2,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import random
+from io import BytesIO
+import base64
 
-df = pd.read_csv('前程无忧网招聘数据.csv')['招聘信息']
+def experience_pic():
+    df = pd.read_csv('前程无忧网招聘数据.csv')['招聘信息']
 
-for i in range(len(df)):
-    str=df.values[i]
-    list=str.split('|')
-    df.values[i]=list[1]
-data = df.value_counts()
-#print(data)
+    for i in range(len(df)):
+        str=df.values[i]
+        list=str.split('|')
+        df.values[i]=list[1]
+    data = df.value_counts()
+    #print(data)
 
-labels = ['无需经验', '1年经验', '2年经验', '3-4年经验', '5-7年经验', '8-9年经验', '10年以上经验']
-nums = [data[i] for i in labels]
-#print(labels)
-#print(nums)
-colors = ['#0000CD', '#FF0000', '#FFD700', '#7FFF00', '#FF1493', '#9400D3', '#87CEFA']
-random.shuffle(colors)
-mpl.rcParams['font.family'] = 'KaiTi'
-plt.style.use('ggplot')
-plt.figure(figsize=(9, 6), dpi=100)
-plt.barh(labels, nums, height=0.5, color=colors)
-plt.title('招聘岗位对工作经验的要求', fontsize=16)
-plt.xlabel('岗位数量', fontsize=12)
-plt.show()
+    labels = ['无需经验', '1年经验', '2年经验', '3-4年经验', '5-7年经验', '8-9年经验', '10年以上经验']
+    nums = [data[i] for i in labels]
+    #print(labels)
+    #print(nums)
+    colors = ['#0000CD', '#FF0000', '#FFD700', '#7FFF00', '#FF1493', '#9400D3', '#87CEFA']
+    random.shuffle(colors)
+    mpl.rcParams['font.family'] = 'KaiTi'
+    plt.style.use('ggplot')
+    plt.figure(figsize=(9, 6), dpi=100)
+    plt.barh(labels, nums, height=0.5, color=colors)
+    plt.title('招聘岗位对工作经验的要求', fontsize=16)
+    plt.xlabel('岗位数量', fontsize=12)
+    #plt.show()
+    fig = BytesIO()
+    plt.savefig(fig, format='png', bbox_inches='tight', pad_inches=0.0)
+    fig.seek(0)
+    fig_png = base64.b64encode(fig.getvalue()) 
+    fig_str = str(fig_png, "utf-8")
+    html = '<img src=\"data:image/png;base64,{}\"/>'.format(fig_str)
+    return html
